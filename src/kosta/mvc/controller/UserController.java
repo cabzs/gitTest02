c3354ddaec5ba688ebb70e47b3bbace6d167f6be
sunjung <sunj20037@gmail.com>
2022-03-03 14:26:37 +0900

    User추가, usercontroller추가

--- src/kosta/mvc/controller/UserController.java
+++ src/kosta/mvc/controller/UserController.java
@@ -1,5 +1,8 @@
 package kosta.mvc.controller;
 
 public class UserController {
+	public void insert() {
+		
+	}
 
 }
---
+++ src/kosta/mvc/dto/User.java
@@ -0,0 +1,8 @@
+package kosta.mvc.dto;
+
+public class User {
+	String name="√÷º±¡§";
+	int age=20;
+	
+
+}
